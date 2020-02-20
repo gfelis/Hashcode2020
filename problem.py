99db24d9c5d1b3fcbf09520b4137a28c83cf777d
Guillem <felisdedios@gmail.com>
2020-02-20 20:53:04 +0100

    Parse done

--- problem.py
+++ problem.py
@@ -1,18 +1,18 @@
-import parsedata.py
 import sys
 
 class Library():
     def __init__(self, books, singuptime, scanrate):
         self.books = books # (list)
-        self.singuptime = singuptime # int
+        self.singUpTime = singuptime # int
         self.scanrate = scanrate # int
+        self.signedUpDate = None
 
 class Problem():
-    def __init__(self):
-        self.libraries # libraries
-        self.scores # score for each library
-        self.bookScores # (list) constant, score for book in position
-        self.time # (int)
+    def __init__(self, libraries, bookScores, time):
+        self.libraries = libraries # libraries
+        self.bookScores = bookScores # (list) constant, score for book in position
+        self.time = time # (int)
+        self.scores = None  # score for each library
         self.date = 0# (int)
 
     def computeTotalScores(self):
@@ -27,10 +27,41 @@ class Problem():
         score = totalBookScores ###TO DO
         return score
 
-    def singUpLibrary(self, index):
+    def liveOneDay(self):
+        self.date += 1
+
+    def signUpLibrary(self, library):
+        if library.signUpTime < (self.time - self.date):
+            library.signedUpDate = self.date + library.signUpTime
+
+    def chooseHighestLibrary(self):
+        highestLibrary = self.libraries[0]
+        for i, score in enumerate(self.scores):
+            if score > highestLibrary.score:
+                highestLibrary = self.libraries[i]
+        return highestLibrary
 
 
+def parsedata(inputfile):
 
+    with open(inputfile, 'r') as fd:
+        first_line = fd.readline().split(" ")
+
+        #n_libraries = first_line[1]
+        total_time = first_line[2]
+
+        bookScores = fd.readline().split(" ")
+        index = 0
+        for line in fd:
+            library_info = line.split(" ")
+            if len(library_info) == 3:
+                index = index + 1
+                print("Llibreria books " + library_info[0])
+                print("Sign Up days " + library_info[1])
+                print("Nª llibres dia " + library_info[2])
+        libraries = []
+        libraries[index] = problem.Library(line, library_info[1], library_info[2])
+        problem.Problem(libraries, bookScores, total_time)
 
 if __name__ == '__main__':
     problem = None
@@ -38,8 +69,6 @@ if __name__ == '__main__':
         print("Usage: ./problem inputfile.txt")
     if len(sys.argv) == 2:
         problem = parsedata(sys.argv[1])
-
-
 
 
 
